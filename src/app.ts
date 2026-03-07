a8df9968bf06ce5d9e60bc0a30f6769455cf035b
mramariam <mramgrpfr@gmail.com>
2026-03-07 15:17:31 +0100

    td2--fini

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,20 @@
 import { Hono } from 'hono'
-
+import { matchs } from 'mock/matchs'
 export const app = new Hono()
 
 app.get('/', (c) => {
-  return c.json({'success':true,'message':'World Cup Ticketing API'})
+  return c.json({ 'success': true, 'message': 'World Cup Ticketing API' })
+})
+app.get('/matchs', (c) => {
+  return c.json({ 'success': true, 'message': 'All matchs', 'data': matchs }, 200);
+})
+app.get('/matchs/:id', (c) => {
+  const matchID = parseInt(c.req.param('id'), 10);
+  const matchURL = matchs.find((m) => m.id === matchID);
+  if (!matchURL) {
+    return c.json({ 'success': false, 'error': "Match " + matchID + " does not exist" }, 404);
+  }
+  return c.json({ 'success': true, 'message': 'Match 1', 'data': { 'id': matchID } }, 200);
 })
 
 
